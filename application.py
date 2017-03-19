2a1cfddb23fa7492609422e9549296241147c16b
Ubuntu <ubuntu@ip-172-31-5-214.ap-south-1.compute.internal>
2017-03-19 21:30:02 +0000

    application wsgi

--- application.py
+++ application.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def application(environ, start_response):
 	return render_template('index.html')
 
 @app.route('/todo/api/v1.0/review/<int:phone_id>/<int:rev_id>', methods=['GET'])
